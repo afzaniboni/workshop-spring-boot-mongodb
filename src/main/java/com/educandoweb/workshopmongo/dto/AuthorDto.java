6bed91fe5c74ba8868906b79e74e189bf60598ea
Andre Zaniboni <afzaniboni@gmail.com>
2020-12-23 23:00:24 -0300

    Projeção dos dados do autor com DTO

---
+++ src/main/java/com/educandoweb/workshopmongo/dto/AuthorDto.java
@@ -0,0 +1,34 @@
+package com.educandoweb.workshopmongo.dto;
+
+import com.educandoweb.workshopmongo.domain.User;
+
+public class AuthorDto {
+
+	private String id;
+	private String name;
+
+	public AuthorDto() {
+	}
+
+	public AuthorDto(User obj) {
+		id = obj.getId();
+		name = obj.getName();
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
